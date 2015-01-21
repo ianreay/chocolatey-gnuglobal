03c79b5903e560b215314be38812fa3705a82b86
Administrator <ian.reay@hitachi-id.com>
2015-01-21 07:04:32 -0700

    initial checkin

---
+++ README.md
@@ -0,0 +1,1 @@
+https://chocolatey.org/ package for easily installing [Gnu Global](http://www.gnu.org/software/global/) on windows systems.
---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'GnuGlobal' # arbitrary name for the package, used in messages
+$url = 'http://adoxa.altervista.org/global/dl.php?f=win32' # download url
+
+$installDir = "$env:SystemDrive\gnuglobal"
+$binDir = $installDir + "\bin" 
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" 
+Install-ChocolateyPath $binDir 'User'
+
